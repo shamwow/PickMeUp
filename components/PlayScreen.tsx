139e7ccd1fbc76dac246852e51e25e5ca80d2ac4
Alexander Fung <alexcfung@gmail.com>
2020-11-21 22:14:37 -0500

    set up play screen

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import {Text, TouchableOpacity} from "react-native";
+import { Audio } from 'expo-av';
+import Toast from 'react-native-easy-toast'
+
+const soundObject = new Audio.Sound();
+
+async function onPlayButtonGenClicked(refs: React.Ref) {
+    // TODO(alex-fung): Read file name from database instead
+    const filePath = "";
+    try {
+        await soundObject.loadAsync(require(filePath));
+        // Your sound is playing!
+        await soundObject.playAsync();
+
+        await soundObject.unloadAsync();
+    } catch (error) {
+        refs.toast.show('hello world!');
+    }
+}
 
 export function PlayScreen() {
+    this.refs = React.createRef();
+
     return (
         <>
+            <TouchableOpacity onPress={onPlayButtonGenClicked(this.refs)}>
+                <Text>Hype Me</Text>
+            </TouchableOpacity>
+            <Toast ref="toast"/>
         </>
     );
 }
