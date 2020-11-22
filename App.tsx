0d971e239e8e38547e6a79daea51bb1eaceadcc1
Alexander Fung <afung2@snapchat.com>
2020-11-21 21:04:13 -0500

    play screen tab

--- App.tsx
+++ App.tsx
@@ -2,13 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { RecordScreen } from './components/RecordScreen';
+import { PlayScreen } from "./components/PlayScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function RecordScreenTab() {
+  return (
+      <View style={styles.container}>
+        <RecordScreen />
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+
+function PlayScreenTab() {
+    return (
+        <View style={styles.container}>
+            <PlayScreen />
+            <StatusBar style="auto" />
+        </View>
+    )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <RecordScreen />
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Record" component={ RecordScreenTab } />
+          <Tab.Screen name="Hype Me Up" component={ PlayScreenTab } />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 
