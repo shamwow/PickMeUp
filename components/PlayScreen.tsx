52a8036383d3c6c3ed1e95da6fa2e1af5af87d58
Alexander Fung <alexcfung@gmail.com>
2020-11-28 05:09:16 -0500

    Play screen can play sound file

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -1,33 +1,40 @@
 import React from "react";
 import {Text, TouchableOpacity} from "react-native";
 import { Audio } from 'expo-av';
-import Toast from 'react-native-easy-toast'
 
 const soundObject = new Audio.Sound();
+soundObject.setOnPlaybackStatusUpdate(async (update) => {
+    console.log("playback status updated!");
+    console.log(update);
+    if (update.isLoaded && update.didJustFinish) {
+        await soundObject.unloadAsync()
+    }
+});
 
-async function onPlayButtonGenClicked(refs: React.Ref) {
-    // TODO(alex-fung): Read file name from database instead
-    const filePath = "";
-    try {
-        await soundObject.loadAsync(require(filePath));
-        // Your sound is playing!
-        await soundObject.playAsync();
+function onPlayButtonGenClicked() {
+    return async () => {
+        // TODO(alex-fung): Read file name from database instead
+        const filePath = "file:///var/mobile/Containers/Data/Application/FF088D24-D732-4200-9F27-EE3783F474B0/Library/Caches/ExponentExperienceData/%2540anonymous%252FPickMeUp-423738e5-8606-403c-a52d-433a02585c1b/AV/recording-6F20CEFD-7CA6-4315-9241-339025C34015.caf";
+        const source = {
+            uri: filePath
+        };
+        try {
+            console.log("playing file!");
+            await soundObject.loadAsync(source);
+            await soundObject.playAsync();
 
-        await soundObject.unloadAsync();
-    } catch (error) {
-        refs.toast.show('hello world!');
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
 export function PlayScreen() {
-    this.refs = React.createRef();
-
     return (
         <>
-            <TouchableOpacity onPress={onPlayButtonGenClicked(this.refs)}>
+            <TouchableOpacity onPress={onPlayButtonGenClicked()}>
                 <Text>Hype Me</Text>
             </TouchableOpacity>
-            <Toast ref="toast"/>
         </>
     );
 }
