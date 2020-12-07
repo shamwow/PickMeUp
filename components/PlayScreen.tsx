3dcdcea5078ec730c0671361f220bafbe5ed10a5
Alexander Fung <alexcfung@gmail.com>
2020-12-06 21:25:45 -0500

    show state to delete that shit

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -1,21 +1,21 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import {Text, TouchableOpacity} from "react-native";
 import { Audio } from 'expo-av';
 import * as SQLite from 'expo-sqlite';
+import Consts from "../consts";
+import {useFocusEffect} from "@react-navigation/native";
 
+const db = SQLite.openDatabase("db.db");
+const state = {
+    showHype: true,
+};
 const soundObject = new Audio.Sound();
-soundObject.setOnPlaybackStatusUpdate(async (update) => {
-    console.log("playback status updated!");
-    console.log(update);
-    if (update.isLoaded && update.didJustFinish) {
-        await soundObject.unloadAsync()
-    }
-});
+let urls: String[] = [];
 
 function onPlayButtonGenClicked() {
     return async () => {
-        // TODO(alex-fung): Read file name from database instead
-        const filePath = "file:///var/mobile/Containers/Data/Application/FF088D24-D732-4200-9F27-EE3783F474B0/Library/Caches/ExponentExperienceData/%2540anonymous%252FPickMeUp-423738e5-8606-403c-a52d-433a02585c1b/AV/recording-6F20CEFD-7CA6-4315-9241-339025C34015.caf";
+        const index = Math.floor(Math.random() * urls.length);
+        const filePath = urls[index];
         const source = {
             uri: filePath
         };
@@ -31,21 +31,50 @@ function onPlayButtonGenClicked() {
 }
 
 export function PlayScreen() {
-    // db name: db.db
-    // table name: recordings
-    useEffect(() => {
+    const [showHype, setShowHype] = useState(true);
+
+    soundObject.setOnPlaybackStatusUpdate(async (update) => {
+        console.log("playback status updated!");
+        console.log(update);
+        if (update.isLoaded && update.didJustFinish) {
+            await soundObject.unloadAsync();
+            //state.showHype = true;
+        } else {
+            setShowHype(!update.isLoaded);
+        }
+        console.log("showHype: " + state.showHype);
+    });
+
+    useFocusEffect(() => {
+        urls = [];
         db.transaction(tx => {
+            tx.executeSql(Consts.CREATE_TABLE_SQL);
             tx.executeSql(
-                "CREATE TABLE IF NOT EXISTS recordings (id integer PRIMARY KEY NOT NULL, path text, date timestamp NOT NULL);"
+                "SELECT * FROM recordings;", [], (_, resultSet) => {
+                    for (let i = 0; i < resultSet.rows.length; i++) {
+                        const item = resultSet.rows.item(i);
+                        console.log(item);
+                        urls.push(item.path);
+                    }
+                }
             );
         });
     });
 
     return (
         <>
-            <TouchableOpacity onPress={onPlayButtonGenClicked()}>
-                <Text>Hype Me</Text>
-            </TouchableOpacity>
+            {
+                showHype &&
+                <TouchableOpacity onPress={onPlayButtonGenClicked()}>
+                    <Text>Hype Me</Text>
+                </TouchableOpacity>
+            }
+            {
+                !showHype &&
+                <TouchableOpacity>
+                    <Text>Delete that shit</Text>
+                </TouchableOpacity>
+            }
         </>
     );
 }
